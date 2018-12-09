2236872ec3613c9e46e525f396b17823deca30a1
ender996 <45601362+ender996@users.noreply.github.com>
2018-12-08 21:48:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -231,22 +231,9 @@ def handle_text_message(event):
             else:
 #                line_bot_api.reply_message(
 #                    event.reply_token, TextSendMessage(text="Untuk melihat list tag ketik '#taglist'"))
-                group = cl.getGroup(msg.to)
-                nama = [contact.mid for contact in group.members]
-                cb = ""
-                cb2 = ""
-                strt = int(0)
-                akh = int(0)
-                for md in nama:
-                    akh = akh + int(6)
-                    cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
-                    strt = strt + int(7)
-                    akh = akh + 1
-                    cb2 += "@nrik \n"
-                cb = (cb[:int(len(cb)-1)])
                 msg.contentType = 0
-                msg.text = cb2
-                msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+                msg.text = '[DL.] PARZIVAL'
+                msg.contentMetadata ={'MENTION':'{"MENTIONEES":['1']}','EMTVER':'4'}
                 try:
                     cl.sendMessage(msg)
 
